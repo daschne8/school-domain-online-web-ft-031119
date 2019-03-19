d21d3577c567d2cb7a7a5ae823a6f3107feb926d
Daniel Schneider <daschne8@gmail.com>
2019-03-19 14:52:25 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,14 @@
 class School
   attr_accessor
-  attr_reader :name
+  attr_reader :name, :roster
+  
   def initialize(name)
     @name = name
   end
+  def add_student(name,age)
+    @roster[age] ? @roster[age] << name : @roster[age] = []
+    @roster[age] << name
+  end
+  def grade
 
 end
